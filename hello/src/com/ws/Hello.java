216ad3e7b6a4a3451acb711959adfa7b1b2a7059
wu-shun <wu_shun@126.com>
2018-08-26 15:15:54 +0800

    Update Hello.java

--- hello/src/com/ws/Hello.java
+++ hello/src/com/ws/Hello.java
@@ -4,11 +4,6 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println(111);
-		System.out.println(222);
-		System.out.println(333);
-		System.out.println(444);
-		System.out.println(666);
-		System.out.println(555);
 
 	}
 
