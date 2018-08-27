869c69bb6d368dd8425a464985773df048e7e777
wu-shun <wu_shun@126.com>
2018-08-27 22:30:24 +0800

    Update Hello.java

--- hello/src/com/ws/Hello.java
+++ hello/src/com/ws/Hello.java
@@ -4,13 +4,6 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println(111);
-		System.out.println(222);
-		System.out.println(444);
-		System.out.println(333);
-		System.out.println("haha");
-		System.out.println("哈哈");
-		System.out.println("or然后我");
-		System.out.println("哈哈oweerowho o");
 	}
 
 }
